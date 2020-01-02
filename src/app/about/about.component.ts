7d3cc0daa866aab3dcee0da0608c7700688942d9
gagan0183 <g.deepsingh1@gmail.com>
2020-01-02 20:56:40 +0530

    stateChanges

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -12,15 +12,8 @@ export class AboutComponent implements OnInit {
   constructor(private db: AngularFirestore) { }
 
   ngOnInit() {
-    this.db.collection('courses').snapshotChanges().subscribe((snaps) => {
-      const courses: Course[] = snaps.map(snap => {
-        return<Course> {
-          id: snap.payload.doc.id,
-          ...snap.payload.doc.data()
-        };
-      })
-      console.log('courses');
-      console.log(courses);
+    this.db.collection('courses').stateChanges().subscribe((snaps) => {
+        console.log(snaps);
     })
   }
 }
